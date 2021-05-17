761fbc1e037366ff545c5cf2353d7bae2bad9d15
Pedro Gaya <pedrohenriquegaya@gmail.com>
2021-05-17 11:49:42 -0300

    added cleaning functionality to main

--- src/main.c
+++ src/main.c
@@ -10,12 +10,14 @@
 
 int main(int argc, char *argv[]) {
     bool isRandom = false;
+    bool shouldClean = false;
     int svalue = 5000;
     int opt;
 
-    while ((opt = getopt(argc, argv, ":s:r")) != -1) {
+    while ((opt = getopt(argc, argv, ":s:rc")) != -1) {
         switch (opt) {
         case 'r': isRandom = true; break;
+        case 'c': shouldClean = true; break;
         case 's': if(atoi(optarg)) { svalue = atoi(optarg); break; }
         case ':': 
                 printf("option [-s] needs a positive integer value\n"); 
@@ -27,6 +29,7 @@ int main(int argc, char *argv[]) {
             printf("Usage: %s [-s size of dataset] [-r]\n", argv[0]);
             printf("-r to randomize the dataset. Default is an ordered dataset.");
             printf("-s to define dataset size. Default is 5000, max is LONG_MAX.");
+            printf("-c to clean old data points before generating new ones.");
             exit(EXIT_FAILURE);
         }
     };
@@ -34,6 +37,17 @@ int main(int argc, char *argv[]) {
     FILE* output = fopen("../logs/logs.txt", "a");
     log_add_fp(output, LOG_TRACE);
 
+
+    if (shouldClean) {
+        remove("../logs/abp_benchmarks/consult/ordered.txt");
+        remove("../logs/abp_benchmarks/consult/random.txt");
+        remove("../logs/abp_benchmarks/insert/ordered.txt");
+        remove("../logs/abp_benchmarks/insert/random.txt");
+        remove("../logs/lsec_benchmarks/consult/ordered.txt");
+        remove("../logs/lsec_benchmarks/consult/random.txt");
+        remove("../logs/lsec_benchmarks/insert/ordered.txt");
+        remove("../logs/lsec_benchmarks/insert/random.txt");
+    }
 
     int* array = generate_DB(svalue, isRandom);
     benchmark_LSE(array, svalue, isRandom);
